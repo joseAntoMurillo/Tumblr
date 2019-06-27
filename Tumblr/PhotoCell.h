7d0e9b4ba68844e3d6280e15babee374b4356a5e
José Antonio Murillo <josemurillo@fb.com>
2019-06-27 12:12:40 -0700

    finished basic

---
+++ Tumblr/PhotoCell.h
@@ -0,0 +1,18 @@
+//
+//  PhotoCell.h
+//  Tumblr
+//
+//  Created by josemurillo on 6/27/19.
+//  Copyright © 2019 josemurillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+
+@end
+
+NS_ASSUME_NONNULL_END
