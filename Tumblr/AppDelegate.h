98f86d52df0e9931ad1fa0b3dc6d1343654a61c7
José Antonio Murillo <josemurillo@fb.com>
2019-06-27 10:28:55 -0700

    Initial Commit

---
+++ Tumblr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tumblr
+//
+//  Created by josemurillo on 6/27/19.
+//  Copyright © 2019 josemurillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tumblr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tumblr
+//
+//  Created by josemurillo on 6/27/19.
+//  Copyright © 2019 josemurillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
